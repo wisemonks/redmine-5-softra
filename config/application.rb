16a1da8aa1b123b22b8a831bcdfbd6e495d923ff
silentm0de <silentm0de@silentm0des-MacBook-Pro.local>
2023-10-11 09:14:10 +0300

    Remove ngrok config.host

--- config/application.rb
+++ config/application.rb
@@ -96,7 +96,5 @@ module RedmineApp
     if File.exist?(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
       instance_eval File.read(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
     end
-
-    config.hosts << "df4f-88-119-95-51.ngrok-free.app"
   end
 end
