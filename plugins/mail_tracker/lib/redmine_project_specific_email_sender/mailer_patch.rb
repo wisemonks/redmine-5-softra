327bdba76e8a7479c5f05deb18a0d2d4194f6f13
silentm0de <silentm0de@silentm0des-MacBook-Pro.local>
2023-10-18 11:09:23 +0300

    MailerPatch uses super instead of alias_method

--- plugins/mail_tracker/lib/redmine_project_specific_email_sender/mailer_patch.rb
+++ plugins/mail_tracker/lib/redmine_project_specific_email_sender/mailer_patch.rb
@@ -2,38 +2,10 @@ module RedmineProjectSpecificEmailSender
   module MailerPatch
     def self.included(base)
       base.send(:include, InstanceMethods)
-      base.class_eval do
-        alias_method :issue_add, :issue_add_with_project_specific_email
-        # alias_method :issue_add_without_project_specific_email, :issue_add
-
-        alias_method :issue_edit, :issue_edit_with_project_specific_email
-        # alias_method :issue_edit_without_project_specific_email, :issue_edit
-
-        alias_method :document_added, :document_added_with_project_specific_email
-        # alias_method :document_added_without_project_specific_email, :document_added
-
-        alias_method :attachments_added, :attachments_added_with_project_specific_email
-        # alias_method :attachments_added_without_project_specific_email, :attachments_added
-
-        alias_method :news_added, :news_added_with_project_specific_email
-        # alias_method :news_added_without_project_specific_email, :news_added
-
-        alias_method :message_posted, :message_posted_with_project_specific_email
-        # alias_method :message_posted_without_project_specific_email, :message_posted
-
-        alias_method :wiki_content_added, :wiki_content_added_with_project_specific_email
-        # alias_method :wiki_content_added_without_project_specific_email, :wiki_content_added
-
-        alias_method :wiki_content_updated, :wiki_content_updated_with_project_specific_email
-        # alias_method :wiki_content_updated_without_project_specific_email, :wiki_content_updated
-
-        alias_method :mail, :mail_with_project_specific_email
-        # alias_method :mail_without_project_specific_email, :mail
-      end
     end
 
     module InstanceMethods
-      def mail_with_project_specific_email(headers={})
+      def mail(headers={})
         if (@project)
           headers['X-Redmine-Project-Specific-Sender'] = @project.email
         end
@@ -42,51 +14,51 @@ module RedmineProjectSpecificEmailSender
         # mail_without_project_specific_email(headers)
       end
 
-      def issue_add_with_project_specific_email(*args)
+      def issue_add(*args)
         @project = args.last.project
         @issue_url_by_project = url_for(:controller => 'issues', :action => 'show', :id => args.first, :host => @project.crm_host_name)
         super
         # issue_add_without_project_specific_email(*args)
       end
 
-      def issue_edit_with_project_specific_email(*args)
+      def issue_edit(*args)
         @project = args.first.journalized.project
         @issue_url_by_project = url_for(:controller => 'issues', :action => 'show', :id => args.first.journalized, :anchor => "change-#{args.first.id}", :host => @project.crm_host_name)
         super
         # issue_edit_without_project_specific_email(*args)
       end
 
-      def document_added_with_project_specific_email(*args)
+      def document_added(*args)
         @project = args.first.project
         super
         # document_added_without_project_specific_email(*args)
       end
 
-      def attachments_added_with_project_specific_email(*args)
+      def attachments_added(*args)
         @project = args.first.first.container.project
         super
         # attachments_added_without_project_specific_email(*args)
       end
 
-      def news_added_with_project_specific_email(*args)
+      def news_added(*args)
         @project = args.first.project
         super
         # news_added_without_project_specific_email(*args)
       end
 
-      def message_posted_with_project_specific_email(*args)
+      def message_posted(*args)
         @project = args.first.board.project
         super
         # message_posted_without_project_specific_email(*args)
       end
 
-      def wiki_content_added_with_project_specific_email(*args)
+      def wiki_content_added(*args)
         @project = args.first.project
         super
         # wiki_content_added_without_project_specific_email(*args)
       end
 
-      def wiki_content_updated_with_project_specific_email(*args)
+      def wiki_content_updated(*args)
         @project = args.first.project
         super
         # wiki_content_updated_without_project_specific_email(*args)
